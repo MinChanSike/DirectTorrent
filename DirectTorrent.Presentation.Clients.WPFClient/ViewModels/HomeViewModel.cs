215506ca5fbe8ec39c0fcb8f7b0308e7e06c2dbb
Matija Cupic <matija.cupic.97@gmail.com>
2015-05-11 12:19:31 +0200

    Fixed property changed broadcast

--- DirectTorrent.Presentation.Clients.WPFClient/ViewModels/HomeViewModel.cs
+++ DirectTorrent.Presentation.Clients.WPFClient/ViewModels/HomeViewModel.cs
@@ -66,13 +66,13 @@ namespace DirectTorrent.Presentation.Clients.WPFClient.ViewModels
         public Sort SelectedSort
         {
             get { return this._selectedSort; }
-            set { this.Set(ref this._selectedSort, value); }
+            set { this.Set(ref this._selectedSort, value, true); }
         }
 
         public Order SelectedOrder
         {
             get { return this._selectedOrder; }
-            set { this.Set(ref this._selectedOrder, value); }
+            set { this.Set(ref this._selectedOrder, value, true); }
         }
         //public byte SelectedMinimumRating
         //{
